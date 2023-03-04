23447e00f695ac207279e1ca8559b463edd977c7
xVlad27x <mvlad27@gmail.com>
2023-03-04 19:40:06 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,156 @@
+// let qestion = confirm('Ты нуб?');
+// console.log(qestion);
+
+// let qestion = prompt('Ты нуб?', '');
+// console.log(qestion);
+
+// let answer = confirm('Тебе 18?');
+// if (answer) {
+//     console.log('Заходи')
+// } else {
+//     console.log('Вали отсюда')
+// }
+
+// const num = 75;
+// if (num < 49) {
+//     console.log('Неправильно')
+// } else if (num > 100) {
+//     console.log('Много')
+// } else {
+//     console.log('Ура')
+// }
+
+// for (let i = 1; i < 8; i++) {
+//     console.log(i);
+// }
+
+// function logging(a, b) {
+//     console.log( a + b )
+// }
+
+// logging(3, 5);
+
+$(document).ready(function(){
+  $('.carousel__inner').slick({
+      speed: 1200,
+      adaptiveHeight: true,
+      prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
+      nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
+      responsive: [
+          {
+              breakpoint: 992,
+              settings: {
+                  dots: true,
+                  arrows: false
+              }
+          }
+      ]
+  });
+
+  $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+    $(this)
+      .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+      .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+  });
+
+  function toggleSlide(item) {
+    $(item).each(function(i) {
+      $(this).on('click', function(e) {
+        e.preventDefault();
+        $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+        $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+      })
+    })
+  };
+
+  toggleSlide('.catalog-item__link');
+  toggleSlide('.catalog-item__back');
+
+  //Modal
+
+  $('[data-modal=consultation]').on('click', function() {
+    $('.overlay, #consultation').fadeIn('slow');
+  });
+  $('.modal__close').on('click', function() {
+    $('.overlay, #consultation, #thanks, #order').fadeOut('slow');
+  });
+
+  $('.button_mini').each(function(i) {
+    $(this).on('click', function() {
+      $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+      $('.overlay, #order').fadeIn('slow');
+    })
+  });
+
+  function validateForms(form) {
+    $(form).validate({
+      rules: {
+        name: {
+            required: true,
+            minlength: 2   
+        },
+        phone: "required",
+        email: {
+          required: true,
+          email: true
+        }
+      },
+      messages: {
+        name: {
+         required: "Пожалуйста, введите своё имя",
+         minlength: jQuery.validator.format("Введите {0} символа!")
+        },
+        phone: "Пожалуйста, введите свой номер телефона",
+        email: {
+          required: "Пожалуйста, введите свою почту",
+          email: "Неправильно введён адрес почты"
+        }
+      }
+    });
+  };
+
+  validateForms('#consultation-form');
+  validateForms('#consultation form');
+  validateForms('#order form');
+  
+  $('input[name=phone]').mask("+3 (999) 999-99-99");
+
+  $('form').submit(function(e) {
+    e.preventDefault();
+
+    if (!$(this).valid()) {
+      return;
+    }
+
+    $.ajax({
+      type: "POST",
+      url: "mailer/smart.php",
+      data: $(this).serialize()
+    }).done(function() {
+      $(this).find("input").val("");
+      $('#consultation, #order').fadeOut();
+      $('.overlay, #thanks').fadeIn('slow');
+
+      $('form').trigger('reset');
+    });
+    return false;
+  });
+
+    //Smooth scroll and pageup
+
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 1600) {
+      $('.pageup').fadeIn();
+    } else {
+      $('.pageup').fadeOut();
+    }
+  });
+
+  $("a[href=#up]").click(function(){
+    const _href = $(this).attr("href");
+    $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+    return false;
+});
+
+new WOW().init();
+});
